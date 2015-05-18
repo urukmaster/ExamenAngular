6730f30e71d4ce0b237031130a28d73c777fb744
mauriciofer <mfernandezm@ucenfotec.ac.cr>
2015-05-17 22:35:56 -0600

    Seleccionar pelicula y tanda

--- Examen 1/resources/js/controllers/MoviesController.js
+++ Examen 1/resources/js/controllers/MoviesController.js
@@ -1,8 +1,7 @@
 'use strict';
 
 /**
- * GeneralController
- * @constructor
+ * Created by Mauricio Fernandez on 17/05/2015.
  */
 
 Array.prototype.remove = function(from, to) {
@@ -12,33 +11,49 @@ Array.prototype.remove = function(from, to) {
 };
 
 var MoviesController = function($scope,$http) {
+    $scope.chosenMovie = {id:"",value:""};
+    $scope.chosenTime = {id:"",value:""};
 
-    $scope.imdbData = {};
     $scope.movies = [
 
         {
-          title: "Avengers",
-          week:  "Lunes - Semana 8"
+            title: "Avengers",
+            description:  "Cuando se trata de Tony Stark para impulsar un programa de mantenimiento de la paz latente, las cosas van mal y depende de los Vengadores para detener al villano Ultron de promulgar sus terribles planes.",
+            time1: "4:00 pm",
+            time2: "7:00 pm",
+            image: "resources/images/avengers.png",
         },
         {
-          title: "Mad Max",
-          week:  "Lunes - Semana 9"
+            title: "Mad Max",
+            description:  "Mad Max forma parte de un grupo que huye por del desierto. Escapan de una ciudadela tiranizada por Immortan Joe, a quien han quitado algo irreemplazable. El Señor de la Guerra moviliza a todos y se genera una Guerra de la Carretera de altas revoluciones.",
+            time1: "12:00 pm",
+            time2: "4:00 pm",
+            image: "resources/images/madmax.png",
         },
         {
-          title: "Fast $ Furious 7",
-          week:  "Lunes - Semana 10"
+            title: "Fast & Furious 7",
+            description:  "Deckard Shaw busca venganza contra Dominic Toretto y su familia por la muerte de su hermano.",
+            time1: "4:30 pm",
+            time2: "8:20 pm",
+            image: "resources/images/fastandfurious.png",
         },
         {
-          title: "Kill the messenger",
-           week:  "Lunes - Semana 11"
+            title: "Kill the messenger",
+            description:  "Un periodista se convierte en el blanco de una campaña de desprestigio que lo impulsa hasta el punto del suicidio despues de que el expone el papel de la CIA en ayudar a armar a los rebeldes en Nicaragua y la importacion de cocaina en California.",
+            time1: "4:00 pm",
+            time2: "7:00 pm",
+            image: "resources/images/kill.png",
         }
     ];
-
-    $scope.actualIndex = 0;
 
     $scope.init = function() {
     };    
 
     $scope.init();
+
+    $scope.chooseMovie = function(movie, index){
+        $scope.chosenMovie.value = movie.title;
+        $scope.chosenTime.value = index;
+    };
 
 };
