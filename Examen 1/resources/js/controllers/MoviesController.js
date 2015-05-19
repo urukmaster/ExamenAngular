8207ce99806a942e7a637ccaa0510a2bf63d0c85
mauriciofer <mfernandezm@ucenfotec.ac.cr>
2015-05-18 18:44:51 -0600

    Se agrega sección de peliculas con ng-models de película y tanda esgogida.

--- Examen 1/resources/js/controllers/MoviesController.js
+++ Examen 1/resources/js/controllers/MoviesController.js
@@ -19,30 +19,53 @@ var MoviesController = function($scope,$http) {
         {
             title: "Avengers",
             description:  "Cuando se trata de Tony Stark para impulsar un programa de mantenimiento de la paz latente, las cosas van mal y depende de los Vengadores para detener al villano Ultron de promulgar sus terribles planes.",
-            time1: "4:00 pm",
-            time2: "7:00 pm",
             image: "resources/images/avengers.png",
+            times: [{
+                id: 1,
+                time: "4:00 pm"
+            }, {
+                id: 2,
+                time: "7:00 pm"
+            }]
         },
         {
             title: "Mad Max",
             description:  "Mad Max forma parte de un grupo que huye por del desierto. Escapan de una ciudadela tiranizada por Immortan Joe, a quien han quitado algo irreemplazable. El Señor de la Guerra moviliza a todos y se genera una Guerra de la Carretera de altas revoluciones.",
-            time1: "12:00 pm",
-            time2: "4:00 pm",
             image: "resources/images/madmax.png",
+            times: [{
+                id: 1,
+                time: "4:30 pm"
+            }, {
+                id: 2,
+                time: "8:00 pm"
+            }]
         },
         {
             title: "Fast & Furious 7",
             description:  "Deckard Shaw busca venganza contra Dominic Toretto y su familia por la muerte de su hermano.",
-            time1: "4:30 pm",
-            time2: "8:20 pm",
             image: "resources/images/fastandfurious.png",
+            times: [{
+                id: 1,
+                time: "3:00 pm"
+            }, {
+                id: 2,
+                time: "10:00 pm"
+            }]
         },
         {
             title: "Kill the messenger",
             description:  "Un periodista se convierte en el blanco de una campaña de desprestigio que lo impulsa hasta el punto del suicidio despues de que el expone el papel de la CIA en ayudar a armar a los rebeldes en Nicaragua y la importacion de cocaina en California.",
-            time1: "4:00 pm",
-            time2: "7:00 pm",
             image: "resources/images/kill.png",
+            times: [{
+                id: 1,
+                time: "2:00 pm"
+            }, {
+                id: 2,
+                time: "5:30 pm"
+            }, {
+                id: 3,
+                time: "9:50"
+            }]
         }
     ];
 
@@ -51,9 +74,9 @@ var MoviesController = function($scope,$http) {
 
     $scope.init();
 
-    $scope.chooseMovie = function(movie, index){
+    $scope.chooseMovie = function(movie, showtime){
         $scope.chosenMovie.value = movie.title;
-        $scope.chosenTime.value = index;
+        $scope.chosenTime.value = showtime.id;
     };
 
 };
